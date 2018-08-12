5d6713d45010bd61c26ca7d3c7d1cedcb22a099b
Amy Kirasack <amykirasack@gmail.com>
2018-08-11 20:25:55 -0400

    Add first couple lines of JSX (not Babel-enabled yet)

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log('script.js is running!')
+
+const dealerTemplate = <p>This is the dealer template</p>;
+const dealerRoot = document.getElementsById('dealer');
+
+const playerOneTemplate = <p>This is the player one template!</p>;
+const playerOneRoot = document.getElementById('playerOne');
+
+ReactDOM.render(dealerTemplate, dealerRoot);
+ReactDOM.render(playerOneTemplate, playerOneRoot);
