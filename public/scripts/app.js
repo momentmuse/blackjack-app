9b29a84d8f72c2abfd21ee540f815111cd225e5a
Amy Kirasack <amykirasack@gmail.com>
2018-08-11 22:05:43 -0400

    Add playerHand, dealerHand, deckValues

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,50 @@
 'use strict';
 
-console.log('script.js is running!');
+console.log('app.js is running!');
+
+var playerHand = [];
+var dealerHand = [];
+
+var deckValues = [{
+    card: 'ace',
+    value: 1 || 11
+}, {
+    card: 'two',
+    value: 2
+}, {
+    card: 'three',
+    value: 3
+}, {
+    card: 'four',
+    value: 4
+}, {
+    card: 'five',
+    value: 5
+}, {
+    card: 'six',
+    value: 6
+}, {
+    card: 'seven',
+    value: 7
+}, {
+    card: 'eight',
+    value: 8
+}, {
+    card: 'nine',
+    value: 9
+}, {
+    card: 'ten',
+    value: 10
+}, {
+    card: 'jack',
+    value: 10
+}, {
+    card: 'queen',
+    value: 10
+}, {
+    card: 'king',
+    value: 10
+}];
 
 var dealerTemplate = React.createElement(
     'div',
@@ -36,3 +80,5 @@ var playerRoot = document.getElementById('player');
 
 ReactDOM.render(dealerTemplate, dealerRoot);
 ReactDOM.render(playerTemplate, playerRoot);
+
+var renderApp = function renderApp() {};
--- src/app.js
+++ src/app.js
@@ -1,4 +1,63 @@
-console.log('script.js is running!')
+console.log('app.js is running!')
+
+const playerHand = [];
+const dealerHand = [];
+
+const deckValues = [
+    {
+        card: 'ace',
+        value: 1 || 11
+    },
+    {
+        card: 'two',
+        value: 2
+    },
+    {
+        card: 'three',
+        value: 3
+    },
+    {
+        card: 'four',
+        value: 4
+    },
+    {
+        card: 'five',
+        value: 5
+    },
+    {
+        card: 'six',
+        value: 6
+    },
+    {
+        card: 'seven',
+        value: 7
+    },
+    {
+        card: 'eight',
+        value: 8
+    },
+    {
+        card: 'nine',
+        value: 9
+    },
+    {
+        card: 'ten',
+        value: 10
+    },
+    {
+        card: 'jack',
+        value: 10
+    },
+    {
+        card: 'queen',
+        value: 10
+    },
+    {
+        card: 'king',
+        value: 10
+    }
+];
+
 
 const dealerTemplate = (
     <div>
@@ -18,3 +77,6 @@ const playerRoot = document.getElementById('player');
 
 ReactDOM.render(dealerTemplate, dealerRoot);
 ReactDOM.render(playerTemplate, playerRoot);
+
+
+const renderApp = () => {};
