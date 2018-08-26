b7aca4400b3fa5826cfa6c8f1b7f321017746249
Amy Kirasack <amykirasack@gmail.com>
2018-08-26 19:48:22 -0400

    Refactor refreshDeck using map

--- src/app.js
+++ src/app.js
@@ -9,269 +9,70 @@ let playerScore = 0;
 
 //function to refresh the deck values after they have been spliced during a game
 //an array of 52 objects with three keys: card, value, and img src
+
 const refreshDeck = () => {
-    deckValues = [
-        {
-            card: 'ace of clubs',
-            value: 11,
-            src: 'images/cards/AC.png'
-        },
-        {
-            card: 'two of clubs',
-            value: 2,
-            src: 'images/cards/2C.png'
-        },
-        {
-            card: 'three of clubs',
-            value: 3,
-            src: 'images/cards/3C.png'
-        },
-        {
-            card: 'four of clubs',
-            value: 4,
-            src: 'images/cards/4C.png'
-        },
-        {
-            card: 'five of clubs',
-            value: 5,
-            src: 'images/cards/5C.png'
-        },
-        {
-            card: 'six of clubs',
-            value: 6,
-            src: 'images/cards/6C.png'
-        },
-        {
-            card: 'seven of clubs',
-            value: 7,
-            src: 'images/cards/7C.png'
-        },
-        {
-            card: 'eight of clubs',
-            value: 8,
-            src: 'images/cards/8C.png'
-        },
-        {
-            card: 'nine of clubs',
-            value: 9,
-            src: 'images/cards/9C.png'
-        },
-        {
-            card: 'ten of clubs',
-            value: 10,
-            src: 'images/cards/10C.png'
-        },
-        {
-            card: 'jack of clubs',
-            value: 10,
-            src: 'images/cards/JC.png'
-        },
-        {
-            card: 'queen of clubs',
-            value: 10,
-            src: 'images/cards/QC.png'
-        },
-        {
-            card: 'king of clubs',
-            value: 10,
-            src: 'images/cards/KC.png'
-        },
-        {
-            card: 'ace of hearts',
-            value: 11,
-            src: 'images/cards/AH.png'
-        },
-        {
-            card: 'two of hearts',
-            value: 2,
-            src: 'images/cards/2H.png'
-        },
-        {
-            card: 'three of hearts',
-            value: 3,
-            src: 'images/cards/3H.png'
-        },
-        {
-            card: 'four of hearts',
-            value: 4,
-            src: 'images/cards/4H.png'
-        },
-        {
-            card: 'five of hearts',
-            value: 5,
-            src: 'images/cards/5H.png'
-        },
-        {
-            card: 'six of hearts',
-            value: 6,
-            src: 'images/cards/6H.png'
-        },
-        {
-            card: 'seven of hearts',
-            value: 7,
-            src: 'images/cards/7H.png'
-        },
-        {
-            card: 'eight of hearts',
-            value: 8,
-            src: 'images/cards/8H.png'
-        },
-        {
-            card: 'nine of hearts',
-            value: 9,
-            src: 'images/cards/9H.png'
-        },
-        {
-            card: 'ten of hearts',
-            value: 10,
-            src: 'images/cards/10H.png'
-        },
-        {
-            card: 'jack of hearts',
-            value: 10,
-            src: 'images/cards/JH.png'
-        },
-        {
-            card: 'queen of hearts',
-            value: 10,
-            src: 'images/cards/QH.png'
-        },
-        {
-            card: 'king of hearts',
-            value: 10,
-            src: 'images/cards/KH.png'
-        },
-        {
-            card: 'ace of spades',
-            value: 11,
-            src: 'images/cards/AS.png'
-        },
-        {
-            card: 'two of spades',
-            value: 2,
-            src: 'images/cards/2S.png'
-        },
-        {
-            card: 'three of spades',
-            value: 3,
-            src: 'images/cards/3S.png'
-        },
-        {
-            card: 'four of spades',
-            value: 4,
-            src: 'images/cards/4S.png'
-        },
-        {
-            card: 'five of spades',
-            value: 5,
-            src: 'images/cards/5S.png'
-        },
-        {
-            card: 'six of spades',
-            value: 6,
-            src: 'images/cards/6S.png'
-        },
-        {
-            card: 'seven of spades',
-            value: 7,
-            src: 'images/cards/7S.png'
-        },
-        {
-            card: 'eight of spades',
-            value: 8,
-            src: 'images/cards/8S.png'
-        },
-        {
-            card: 'nine of spades',
-            value: 9,
-            src: 'images/cards/9S.png'
-        },
-        {
-            card: 'ten of spades',
-            value: 10,
-            src: 'images/cards/10S.png'
-        },
-        {
-            card: 'jack of spades',
-            value: 10,
-            src: 'images/cards/JS.png'
-        },
-        {
-            card: 'queen of spades',
-            value: 10,
-            src: 'images/cards/QS.png'
-        },
-        {
-            card: 'king of spades',
-            value: 10,
-            src: 'images/cards/KS.png'
-        },
-        {
-            card: 'ace of diamonds',
-            value: 11,
-            src: 'images/cards/AD.png'
-        },
-        {
-            card: 'two of diamonds',
-            value: 2,
-            src: 'images/cards/2D.png'
-        },
-        {
-            card: 'three of diamonds',
-            value: 3,
-            src: 'images/cards/3D.png'
-        },
-        {
-            card: 'four of diamonds',
-            value: 4,
-            src: 'images/cards/4D.png'
-        },
-        {
-            card: 'five of diamonds',
-            value: 5,
-            src: 'images/cards/5D.png'
-        },
-        {
-            card: 'six of diamonds',
-            value: 6,
-            src: 'images/cards/6D.png'
-        },
-        {
-            card: 'seven of diamonds',
-            value: 7,
-            src: 'images/cards/7D.png'
-        },
-        {
-            card: 'eight of diamonds',
-            value: 8,
-            src: 'images/cards/8D.png'
-        },
-        {
-            card: 'nine of diamonds',
-            value: 9,
-            src: 'images/cards/9D.png'
-        },
-        {
-            card: 'ten of diamonds',
-            value: 10,
-            src: 'images/cards/10D.png'
-        },
-        {
-            card: 'jack of diamonds',
-            value: 10,
-            src: 'images/cards/JD.png'
-        },
-        {
-            card: 'queen of diamonds',
-            value: 10,
-            src: 'images/cards/QD.png'
-        },
-        {
-            card: 'king of diamonds',
-            value: 10,
-            src: 'images/cards/KD.png'
-        }
-    ];
+    return [
+        [11, 'AC'],
+        [2, '2C'],
+        [3, '3C'],
+        [4, '4C'],
+        [5, '5C'],
+        [6, '6C'],
+        [7, '7C'],
+        [8, '8C'],
+        [9, '9C'],
+        [10, '10C'],
+        [10, 'JC'],
+        [10, 'QC'],
+        [10, 'KC'],
+
+        [11, 'AH'],
+        [2, '2H'],
+        [3, '3H'],
+        [4, '4H'],
+        [5, '5H'],
+        [6, '6H'],
+        [7, '7H'],
+        [8, '8H'],
+        [9, '9H'],
+        [10, '10H'],
+        [10, 'JH'],
+        [10, 'QH'],
+        [10, 'KH'],
+
+        [11, 'AS'],
+        [2, '2S'],
+        [3, '3S'],
+        [4, '4S'],
+        [5, '5S'],
+        [6, '6S'],
+        [7, '7S'],
+        [8, '8S'],
+        [9, '9S'],
+        [10, '10S'],
+        [10, 'JS'],
+        [10, 'QS'],
+        [10, 'KS'],
+
+        [11, 'AD'],
+        [2, '2D'],
+        [3, '3D'],
+        [4, '4D'],
+        [5, '5D'],
+        [6, '6D'],
+        [7, '7D'],
+        [8, '8D'],
+        [9, '9D'],
+        [10, '10D'],
+        [10, 'JD'],
+        [10, 'QD'],
+        [10, 'KD']
+    ].map((card) => {
+        return {
+                value: card[0],
+                src: `images/cards/${card[1]}.png`
+                }
+    });
 };
 
 //resets game status, deckValues, and hands, to values on startup
